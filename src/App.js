5bf4dbf0e5e51c8061a21c0089ea23354d6a322f
andressa-leite <dre_tl@hotmail.com>
2022-11-09 07:58:36 -0300

    implementado drawer no menu além do home e products no menu

--- src/App.js
+++ src/App.js
@@ -2,44 +2,25 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 import List from './componentes/List/List';
-//import { Router, Switch, Route, Link } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Product from './componentes/Product/Product';
-import Nav from './componentes/Nav/Nav';
-
-
-
+import PersistentDrawerLeft from './componentes/Nav/Nav';
 
 function App() {
     return (
     <div className="App">
-      <Nav />
-      
 
       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-          </ul>
-        </nav>
+      
+        <PersistentDrawerLeft />
 
         <Routes>
-          <Route path="/products"  element={<List />}>
-            
+          <Route path="/products"  element={<List />}>  
           </Route>
           <Route path="/products/:id"  element={<Product />}>
-            
           </Route>
-
-    
         </Routes>
-      </div>
+    
     </Router>
 
 
